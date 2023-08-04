645524361467b408eac450e3a4fa3c138e196a8c
lidiee <lidieee@naver.com>
2023-08-04 22:42:31 +0900

    헤더

---
+++ 01 Blue Apron/script/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let scrollY = $(window).scrollTop();
+        if(scrollY > 500){
+            $('#header_top').addClass("fixed")
+        }else{
+            $('#header_top').removeClass("fixed")
+        }
+        if(scrollY > 500){
+            $('.gnb a').addClass("fixed")
+        }else{
+            $('.gnb a').removeClass("fixed")
+        }
+        if(scrollY > 500){
+            $('.sign .sign_in a').addClass("fixed")
+        }else{
+            $('.sign .sign_in a').removeClass("fixed")
+        }
+    })
+})
