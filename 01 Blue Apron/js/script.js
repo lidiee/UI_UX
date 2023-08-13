7b9ada486d03d34858030eb0304a46eca36a68f1
lidiee <lidieee@naver.com>
2023-08-13 23:34:07 +0900

    윈도우 리사이즈, 토글 버튼 스트립트 작성

--- 01 Blue Apron/js/script.js
+++ 01 Blue Apron/js/script.js
@@ -30,4 +30,24 @@ $(document).ready(function(){
         slider1.goToPrevSlide();
     });
 
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class");
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            $(".toggle").addClass("active");
+            // document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            $(".toggle").removeClass("active");
+            // document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
+
 })
