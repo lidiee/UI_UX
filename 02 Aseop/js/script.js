79cd362662ff04a2b5b15dacbc598631aee486eb
lidiee <lidieee@naver.com>
2023-08-15 22:44:51 +0900

    풀페이지 기본 작업

---
+++ 02 Aseop/js/script.js
@@ -0,0 +1,28 @@
+console.log("Hi :-)")
+
+$(function(){
+    $("#fullpage").fullpage({
+        menu:".navbar-nav",
+        anchors:["home","read","service","store"],
+        navigation:true,
+        navigationPosition:'fp-right',
+        navigationTooltips: ['Aēsop', 'Read','Services','Store'],
+        responsiveWidth:768,
+
+        afterLoad:function(anchorslink,index){
+            console.log(`${anchorslink},${index}`)
+
+            if(index == 1){
+                $(".view01").addClass("on")
+            }else{
+                $(".view01").removeClass("on")
+            }
+
+            if(index == 2){
+                $(".view02").addClass("on")
+            }else{
+                $(".view02").removeClass("on")
+            }
+        }
+    })
+})
