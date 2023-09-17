d3b448b441841f4943a588cae8b51ce2e1140354
lidiee <lidieee@naver.com>
2023-09-17 23:20:57 +0900

    스크립트 파일 주석 처리

--- 03 Parallax/js/script.js
+++ 03 Parallax/js/script.js
@@ -1,12 +1,12 @@
-$(function(){
-    let layer = document.querySelectorAll(".layer")
+// $(function(){
+//     let layer = document.querySelectorAll(".layer")
 
-    window.addEventListener("scroll",function(){
-        let scroll = window.pageYOffset;
+//     window.addEventListener("scroll",function(){
+//         let scroll = window.pageYOffset;
 
-        layer.forEach(function(item,index){
-            let speed = item.dataset.speed;
-            item.style.transform = `translate3d(0,${scroll/speed*4}px,0)`
-        })
-    })
-})
+//         layer.forEach(function(item,index){
+//             let speed = item.dataset.speed;
+//             item.style.transform = `translate3d(0,${scroll/speed*4}px,0)`
+//         })
+//     })
+// })
