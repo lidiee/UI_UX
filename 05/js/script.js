577ee3d5a1da9cd4ff9e8107eadd0819861af9f3
lidiee <lidieee@naver.com>
2023-11-10 16:14:00 +0900

    회원가입

--- 05/js/script.js
+++ 05/js/script.js
@@ -9,4 +9,11 @@ $(document).ready(function(){
     $(".prevbtn").click(function(){
         slider1.goToPrevSlide();
     });
+
 })
+function toggle(source) {
+    checkboxes = document.getElementsByName('foo');
+    for(var i=0, n=checkboxes.length; i<n; i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
