5277c97d08d1cb922837244a77c930523ddc538b
lidiee <lidieee@naver.com>
2023-11-21 23:40:11 +0900

    Update script.js

--- 05/js/script.js
+++ 05/js/script.js
@@ -11,9 +11,10 @@ $(document).ready(function(){
     });
 
 })
-function toggle(source) {
-    checkboxes = document.getElementsByName('foo');
-    for(var i=0, n=checkboxes.length; i<n; i++) {
-        checkboxes[i].checked = source.checked;
-    }
-}
+// function toggle(source) {
+//     checkboxes = document.getElementsByName('foo');
+//     for(var i=0, n=checkboxes.length; i<n; i++) {
+//         checkboxes[i].checked = source.checked;
+//     }
+// }
+
