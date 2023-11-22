e8ab170b0f74cd0ea72c0a546348fbc61cb2ef6a
lidiee <lidieee@naver.com>
2023-11-22 22:38:19 +0900

    체크박스

--- 05/js/script.js
+++ 05/js/script.js
@@ -18,3 +18,28 @@ $(document).ready(function(){
 //     }
 // }
 
+var $checkAll = $('#checkAll');
+$checkAll.change(function () {
+    var $this = $(this);
+    var checked = $this.prop('checked');
+    $('input[name="foo"]').prop('checked', checked);
+});
+var boxes = $('input[name="foo"]');
+boxes.change(function () {
+//   var checkAll = true;
+//   var count = boxes.length;
+//   var i = 0;
+//   for (; i < count; i++) {
+//     if (!foo[i].checked) {
+//       checkAll = false;
+//       break;
+//     }
+//   }
+
+var boxLength = boxes.length;
+  // 체크된 체크박스 갯수를 확인하기 위해 :checked 필터를 사용하여 체크박스만 선택한 후 length 프로퍼티를 확인
+  var checkedLength = $('input[name="foo"]:checked').length;
+  var checkAll = (boxLength == checkedLength);
+
+  $sportAll.prop('checked', checkAll);
+});
