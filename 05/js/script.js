8b6ab0e21b9dc6406773df8f7bcead7cbd4abc59
lidiee <lidieee@naver.com>
2023-11-23 16:30:31 +0900

    전체선택 수정

--- 05/js/script.js
+++ 05/js/script.js
@@ -11,35 +11,23 @@ $(document).ready(function(){
     });
 
 })
-// function toggle(source) {
-//     checkboxes = document.getElementsByName('foo');
-//     for(var i=0, n=checkboxes.length; i<n; i++) {
-//         checkboxes[i].checked = source.checked;
-//     }
-// }
+function toggle(source) {
+    checkboxes = document.getElementsByName('foo');
+    for(var i=0, n=checkboxes.length; i<n; i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
 
-var $checkAll = $('#checkAll');
-$checkAll.change(function () {
-    var $this = $(this);
-    var checked = $this.prop('checked');
-    $('input[name="foo"]').prop('checked', checked);
-});
-var boxes = $('input[name="foo"]');
-boxes.change(function () {
-//   var checkAll = true;
-//   var count = boxes.length;
-//   var i = 0;
-//   for (; i < count; i++) {
-//     if (!foo[i].checked) {
-//       checkAll = false;
-//       break;
-//     }
-//   }
+function checkAll(source) {
+    var checkboxes = document.getElementsByName('foo');
+    var allChecked = true;
 
-var boxLength = boxes.length;
-  // 체크된 체크박스 갯수를 확인하기 위해 :checked 필터를 사용하여 체크박스만 선택한 후 length 프로퍼티를 확인
-  var checkedLength = $('input[name="foo"]:checked').length;
-  var checkAll = (boxLength == checkedLength);
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        if (!checkboxes[i].checked) {
+            allChecked = false;
+            break;
+        }
+    }
 
-  $sportAll.prop('checked', checkAll);
-});
+    document.getElementById('check').checked = allChecked;
+}
